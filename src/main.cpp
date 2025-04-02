40f32c35c3f06c2bf5fbca6eb84add7809ad6557
flyman150 <2410757683@qq.com>
2025-04-02 14:13:12 +0800

    将函数和变量名改为驼峰命名法

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,10 @@ void printUsage(const char* programName) {
 }
 
 struct ClientConfig {
-    std::string server_ip = "127.0.0.1";
-    int server_port = 8888;
-    int client_count = 10;
-    int message_interval = 2000;
+    std::string serverIp = "127.0.0.1";
+    int serverPort = 8888;
+    int clientCount = 10;
+    int messageInterval = 2000;
 };
 
 ClientConfig parseArguments(int argc, char* argv[]) {
@@ -36,28 +36,28 @@ ClientConfig parseArguments(int argc, char* argv[]) {
             exit(0);
         } else if (arg == "-s" || arg == "--server") {
             if (i + 1 < argc) {
-                config.server_ip = argv[++i];
+                config.serverIp = argv[++i];
             }
         } else if (arg == "-p" || arg == "--port") {
             if (i + 1 < argc) {
-                config.server_port = std::atoi(argv[++i]);
-                if (config.server_port <= 0 || config.server_port > 65535) {
+                config.serverPort = std::atoi(argv[++i]);
+                if (config.serverPort <= 0 || config.serverPort > 65535) {
                     std::cerr << "错误：端口号必须在1-65535之间" << std::endl;
                     exit(1);
                 }
             }
         } else if (arg == "-c" || arg == "--clients") {
             if (i + 1 < argc) {
-                config.client_count = std::atoi(argv[++i]);
-                if (config.client_count <= 0) {
+                config.clientCount = std::atoi(argv[++i]);
+                if (config.clientCount <= 0) {
                     std::cerr << "错误：客户端数量必须大于0" << std::endl;
                     exit(1);
                 }
             }
         } else if (arg == "-i" || arg == "--interval") {
             if (i + 1 < argc) {
-                config.message_interval = std::atoi(argv[++i]);
-                if (config.message_interval < 100) {
+                config.messageInterval = std::atoi(argv[++i]);
+                if (config.messageInterval < 100) {
                     std::cerr << "错误：消息间隔必须大于100毫秒" << std::endl;
                     exit(1);
                 }
@@ -69,21 +69,21 @@ ClientConfig parseArguments(int argc, char* argv[]) {
 }
 
 // 全局变量用于控制所有客户端
-std::atomic<bool> g_running(true);
+std::atomic<bool> gRunning(true);
 
 // 客户端线程函数
-void clientThread(const int client_id, 
-                 const std::string& server_ip, 
-                 const int server_port,
-                 const int message_interval) {
+void clientThread(const int clientId, 
+                 const std::string& serverIp, 
+                 const int serverPort,
+                 const int messageInterval) {
     // 创建TCP客户端实例
-    std::vector<std::string> server_ips = {server_ip};
-    std::vector<int> server_ports = {server_port};
-    TcpClient client(server_ips, server_ports, 3000);
+    std::vector<std::string> serverIps = {serverIp};
+    std::vector<int> serverPorts = {serverPort};
+    TcpClient client(serverIps, serverPorts, 3000);
     
     // 设置连接状态回调
-    client.setConnectionCallback([client_id](bool connected) {
-        std::cout << "客户端 " << client_id << " 连接状态: " 
+    client.setConnectionCallback([clientId](bool connected) {
+        std::cout << "客户端 " << clientId << " 连接状态: " 
                   << (connected ? "已连接" : "已断开") << std::endl;
     });
     
@@ -91,16 +91,16 @@ void clientThread(const int client_id,
     client.start();
     
     // 主循环
-    while (g_running) {
+    while (gRunning) {
         if (client.isConnected()) {
-            std::string message = "客户端 " + std::to_string(client_id) + " 发送的消息";
+            std::string message = "客户端 " + std::to_string(clientId) + " 发送的消息";
             if (client.sendMessage(message)) {
-                std::cout << "客户端 " << client_id << " 消息发送成功" << std::endl;
+                std::cout << "客户端 " << clientId << " 消息发送成功" << std::endl;
             } else {
-                std::cout << "客户端 " << client_id << " 消息发送失败" << std::endl;
+                std::cout << "客户端 " << clientId << " 消息发送失败" << std::endl;
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(message_interval));
+        std::this_thread::sleep_for(std::chrono::milliseconds(messageInterval));
     }
     
     // 停止客户端
@@ -112,20 +112,20 @@ int main(int argc, char* argv[]) {
     ClientConfig config = parseArguments(argc, argv);
     
     std::cout << "启动配置:\n"
-              << "服务器IP: " << config.server_ip << "\n"
-              << "服务器端口: " << config.server_port << "\n"
-              << "客户端数量: " << config.client_count << "\n"
-              << "消息发送间隔: " << config.message_interval << "ms\n"
+              << "服务器IP: " << config.serverIp << "\n"
+              << "服务器端口: " << config.serverPort << "\n"
+              << "客户端数量: " << config.clientCount << "\n"
+              << "消息发送间隔: " << config.messageInterval << "ms\n"
               << std::endl;
     
-    std::vector<std::thread> client_threads;
+    std::vector<std::thread> clientThreads;
     
     // 创建并启动所有客户端线程
-    for (int i = 0; i < config.client_count; ++i) {
-        client_threads.emplace_back(clientThread, i, 
-                                  config.server_ip, 
-                                  config.server_port,
-                                  config.message_interval);
+    for (int i = 0; i < config.clientCount; ++i) {
+        clientThreads.emplace_back(clientThread, i, 
+                                  config.serverIp, 
+                                  config.serverPort,
+                                  config.messageInterval);
     }
     
     // 等待用户输入来停止所有客户端
@@ -133,10 +133,10 @@ int main(int argc, char* argv[]) {
     std::cin.get();
     
     // 设置停止标志
-    g_running = false;
+    gRunning = false;
     
     // 等待所有客户端线程结束
-    for (auto& thread : client_threads) {
+    for (auto& thread : clientThreads) {
         if (thread.joinable()) {
             thread.join();
         }
